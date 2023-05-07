d41d92c590ab37c495081a55fd87e645d6401eaf
Satadru Bhattacharjee <satadru.bhattacharjee@intigral.net>
2023-05-07 20:02:07 +0400

    Moving the Flask dependencies inside the app folder

--- apps/langchain_flask_api/app.py
+++ apps/langchain_flask_api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+#print("Flask version: " + Flask.__version__)
+
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! "

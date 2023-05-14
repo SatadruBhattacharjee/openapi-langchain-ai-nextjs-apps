d291d1b6a8930b56e048c24aa26b09cb257cb44b
Satadru Bhattacharjee <satadru.bhattacharjee@intigral.net>
2023-05-14 23:19:28 +0400

    CSS color palette tuning to gold

--- libs/ui/src/lib/components/sidebar/sidebar.tsx
+++ libs/ui/src/lib/components/sidebar/sidebar.tsx
@@ -122,9 +122,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles['sidebar-header']}>
-        <div className={styles['sidebar-title']}>ChatGPT Next</div>
+        <div className={styles['sidebar-title']}>Enterprise ChatGPT Next</div>
         <div className={styles['sidebar-sub-title']}>
-          Build your own AI assistant.
+          Build your Enterprise AI assistant.
         </div>
         <div className={styles['sidebar-logo'] + ' no-dark'}>
           <ChatGptIcon />

b6e726d2273995d8955491bbfb40bb48c3fed7d7
Satadru Bhattacharjee <satadru.bhattacharjee@intigral.net>
2023-05-21 11:49:47 +0400

    renamed the app to chat-ui

--- apps/chat-ui/jest.config.ts
+++ apps/chat-ui/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'finace-report-pdf-chatgpt',
+  displayName: 'chat-ui',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/finace-report-pdf-chatgpt',
+  coverageDirectory: '../../coverage/apps/chat-ui',
 };
